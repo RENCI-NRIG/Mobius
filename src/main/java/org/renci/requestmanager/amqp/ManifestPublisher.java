2046ff53508d806e56289ff8110f22f47e06e57e
anirban <anriban@users.noreply.github.com>
2014-10-03 19:10:50 +0000

    testing sending manifest info to SQ

--- src/main/java/org/renci/requestmanager/amqp/ManifestPublisher.java
+++ src/main/java/org/renci/requestmanager/amqp/ManifestPublisher.java
@@ -6,6 +6,11 @@
 
 package org.renci.requestmanager.amqp;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import org.json.simple.JSONObject;
+
 /**
  *
  * @author anirban
@@ -13,5 +18,66 @@ package org.renci.requestmanager.amqp;
 public class ManifestPublisher {
     
     // This class is responsible for publishing manifests to an exchange
+    private static final String EXCHANGE_NAME = "testManifestExchange";
+
+    public static void main(String[] argv) {
+        
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("gaul.isi.edu");
+            factory.setPort(5671);
+            factory.useSslProtocol();
+            factory.setUsername("anirban");
+            factory.setPassword("adamant123");
+            
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+            String routingKey = getRouting(argv);
+            String message = buildMessageManifestResponse();
+
+            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes());
+            System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
+        }
+        catch  (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) {
+              try {
+                connection.close();
+              }
+              catch (Exception ignore) {}
+            }
+        }
+        
+    }
+
+    private static String getRouting(String[] strings){
+        if (strings.length < 1)
+            return "testSlice.0xcdfvgh";
+        return strings[0];
+    }
+
+    private static String buildMessageManifestResponse(){
+        
+        // Build a test JSON message
+        JSONObject obj = new JSONObject();
+	// mandatory
+        obj.put("response_orcaSliceID", "testSlice");
+        obj.put("response_sliceStatus", "ready");
+        obj.put("response_numWorkersReady", 4);
+        
+        System.out.println("JSON response = \n" + obj.toJSONString());
+                
+        return obj.toJSONString();
+        
+    }    
     
 }
---
+++ src/main/java/org/renci/requestmanager/amqp/ManifestTestSubscriber.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.renci.requestmanager.amqp;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ *
+ * @author anirban
+ */
+public class ManifestTestSubscriber {
+    
+  private static final String EXCHANGE_NAME = "testManifestExchange";
+
+  public static void main(String[] argv) {
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("gaul.isi.edu");
+            factory.setPort(5671);
+            factory.useSslProtocol();
+            factory.setUsername("anirban");
+            factory.setPassword("adamant123");
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+            String queueName = channel.queueDeclare().getQueue();
+ 
+            String bindingKey = "testSlice.*";             
+            channel.queueBind(queueName, EXCHANGE_NAME, bindingKey);
+            
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(queueName, true, consumer);
+
+            while (true) {
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                String message = new String(delivery.getBody());
+                String routingKey = delivery.getEnvelope().getRoutingKey();
+
+                System.out.println(" [x] Received '" + routingKey + "':'" + message + "'");   
+            }
+        }
+        catch  (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) {
+            try {
+                connection.close();
+            }
+            catch (Exception ignore) {}
+            }
+        }
+        
+    }    
+    
+    
+}
