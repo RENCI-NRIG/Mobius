1eb27d86373a5bb82ebd1a899fb175d40c60400d
anirban <anriban@users.noreply.github.com>
2016-11-01 18:26:28 +0000

    making AMQP server configurable

--- src/main/java/org/renci/requestmanager/amqp/SetupAMQPConnection.java
+++ src/main/java/org/renci/requestmanager/amqp/SetupAMQPConnection.java
@@ -8,13 +8,14 @@ package org.renci.requestmanager.amqp;
 import com.rabbitmq.client.ConnectionFactory;
 import java.util.Properties;
 import org.apache.log4j.Logger;
+import org.renci.requestmanager.RMConstants;
 import org.renci.requestmanager.RMState;
 
 /**
  *
  * @author anirban
  */
-public class SetupAMQPConnection {
+public class SetupAMQPConnection implements RMConstants{
 
     
     protected Logger logger = null;
@@ -35,14 +36,72 @@ public class SetupAMQPConnection {
         RMState rmState = RMState.getInstance();
         
         ConnectionFactory factory = rmState.getFactory();
-	//factory.setHost("gaul.isi.edu");
-        factory.setHost("stewie.isi.edu");
-        factory.setPort(5671);
-        factory.useSslProtocol();
-        factory.setUsername("anirban");
-        //factory.setPassword("adamant123");
-        factory.setPassword("panorama123");
-        factory.setVirtualHost("panorama");
+        
+        if(rmProps.getProperty(AMQP_SERVER_NAME_PROP) != null){
+            factory.setHost(rmProps.getProperty(AMQP_SERVER_NAME_PROP));
+            logger.info("AMQP host: " + rmProps.getProperty(AMQP_SERVER_NAME_PROP));
+        }
+        else{
+            factory.setHost("stewie.isi.edu");
+            logger.info("AMQP host: " + "stewie.isi.edu");
+        }
+        
+        if(rmProps.getProperty(AMQP_SERVER_PORT_PROP) != null){
+            factory.setPort(Integer.parseInt(rmProps.getProperty(AMQP_SERVER_PORT_PROP)));
+            logger.info("AMQP port: " + Integer.parseInt(rmProps.getProperty(AMQP_SERVER_PORT_PROP)));
+        }
+        else{
+            factory.setPort(5671); 
+            logger.info("AMQP port: 5671");
+        }
+        
+        if(rmProps.getProperty(AMQP_SERVER_SSL_PROP) != null){
+            String useSSLString = rmProps.getProperty(AMQP_SERVER_SSL_PROP);
+            if(useSSLString.equalsIgnoreCase("true")){
+                factory.useSslProtocol();
+                logger.info("AMQP useSSL: " + "true");
+            }
+            else{
+                logger.info("AMQP useSSL: " + "false");
+            }
+        }
+        else{
+            logger.info("AMQP useSSL: " + "false");
+        }
+        
+        if(rmProps.getProperty(AMQP_USER_NAME_PROP) != null){
+            factory.setUsername(rmProps.getProperty(AMQP_USER_NAME_PROP));
+            logger.info("AMQP user: " + rmProps.getProperty(AMQP_USER_NAME_PROP));
+        }
+        else{
+            factory.setUsername("anirban");
+            logger.info("AMQP user: " + "anirban");
+        }
+        
+        if(rmProps.getProperty(AMQP_USER_PASSWORD_PROP) != null){
+            factory.setPassword(rmProps.getProperty(AMQP_USER_PASSWORD_PROP));
+            logger.info("AMQP password: " + rmProps.getProperty(AMQP_USER_PASSWORD_PROP));
+        }
+        else{
+            factory.setPassword("panorama123");
+            logger.info("AMQP password: " + "panorama123");
+        }
+        
+        if(rmProps.getProperty(AMQP_VIRTUAL_HOST_PROP) != null){
+            factory.setVirtualHost(rmProps.getProperty(AMQP_VIRTUAL_HOST_PROP));
+            logger.info("AMQP virtualhost: " + rmProps.getProperty(AMQP_VIRTUAL_HOST_PROP));
+        }
+        else{
+            factory.setVirtualHost("panorama");
+            logger.info("AMQP virtualhost: " + "panorama");
+        }
+        
+//        factory.setHost("stewie.isi.edu");
+//        factory.setPort(5671);
+//        factory.useSslProtocol();
+//        factory.setUsername("anirban");
+//        factory.setPassword("panorama123");
+//        factory.setVirtualHost("panorama");
         
         rmState.setFactory(factory);
         
