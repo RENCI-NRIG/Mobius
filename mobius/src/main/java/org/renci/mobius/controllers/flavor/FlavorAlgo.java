528e9dbac639e0d454a6fb40e25fb2304625ea00
Komal Thareja <kthar10@renci.org>
2019-01-24 09:42:06 -0500

    changes for inputs from demo

---
+++ mobius/src/main/java/org/renci/mobius/controllers/flavor/FlavorAlgo.java
@@ -0,0 +1,9 @@
+package org.renci.mobius.controllers.flavor;
+
+import java.util.List;
+
+public interface FlavorAlgo {
+    public static List<String> determineFlavors(Integer cpus, Integer ramPerCpus, Integer diskPerCpus, boolean isCoallocate) {
+        return null;
+    }
+}
