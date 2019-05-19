83b4442d4f9f34187d3efd4563e2ae8a48545433
Komal Thareja <kthar10@renci.org>
2019-05-19 11:57:45 -0400

    fix flavor for extra large

--- mobius/src/main/java/org/renci/mobius/controllers/exogeni/ExogeniFlavorAlgo.java
+++ mobius/src/main/java/org/renci/mobius/controllers/exogeni/ExogeniFlavorAlgo.java
@@ -21,8 +21,8 @@ class ExogeniFlavorAlgo {
         Small(1, 11264, 1024, "XO Small"),
         Medium(1, 26624, 3072, "XO Medium"),
         Large(2, 52224, 6144, "XO Large"),
-        ExtraLarge(4, 76800, 12288, "XO Extra Large"),
-        BareMetal(20, 49152, 102400, "ExoGENI Bare-Metal");
+        ExtraLarge(4, 76800, 12288, "XO Extra large"),
+        BareMetal(20, 49152, 102400, "ExoGENI Bare-metal");
 
         private Flavor(Integer cpus, Integer diskSpace, Integer ram, String name) {
             this.cpus = cpus;
