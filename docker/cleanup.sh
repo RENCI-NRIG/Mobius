504ff047805425769a146df057bbf92968e2bba4
Komal Thareja <kthar10@renci.org>
2019-08-29 08:56:27 -0400

    changes to support network request between Exogeni sites

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-docker stop database notification mobius rabbitmq
-docker rm database notification mobius rabbitmq
-docker rmi postgres:10 rencinrig/mobius:1.0.0-SNAPSHOT rabbitmq:management  rencinrig/notification:1.0.0-SNAPSHOT
+docker stop database notification mobius rabbitmq sdxserver
+docker rm database notification mobius rabbitmq sdxserver
+docker rmi postgres:10 rencinrig/mobius:1.0.0-SNAPSHOT rabbitmq:management  rencinrig/notification:1.0.0-SNAPSHOT rencinrig/sdxserver:0.1-SNAPSHOT
 #docker rmi rencinrig/mobius:1.0.0-SNAPSHOT rabbitmq:management  rencinrig/notification:1.0.0-SNAPSHOT
 #docker network rm $(docker network ls --format "{{.ID}}")
 docker volume rm $(docker volume ls -qf dangling=true)
