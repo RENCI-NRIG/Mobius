a3625ce8286467029d14f1f1ad91d33d2ceb7b77
Komal Thareja <kthar10@renci.org>
2019-09-16 10:54:36 -0400

    support for monitoring in postboot script

---
+++ mobius/test/chtest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -X POST -i "localhost:8080/mobius/workflow?workflowID=abcd-5679" -H "accept: application/json"
+curl -X POST -i "localhost:8080/mobius/compute?workflowID=abcd-5679" -H "accept: application/json" -H "Content-Type: application/json"  -d @chcomputeMasterPrivNetwork.json
+curl -X POST -i "localhost:8080/mobius/compute?workflowID=abcd-5679" -H "accept: application/json" -H "Content-Type: application/json"  -d @computeMaster.json
