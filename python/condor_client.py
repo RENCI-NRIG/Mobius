4be332c49dec0baf201521fe7dea480f6db20011
Komal Thareja <kthar10@renci.org>
2020-03-09 09:22:11 -0400

    changes for network monitoring

--- python/condor_client.py
+++ python/condor_client.py
@@ -20,7 +20,7 @@ import json
 import argparse
 import subprocess
 import socket
-
+from ssl import create_default_context, Purpose
 
 from mobius import *
 from comet_common_iface import *
@@ -272,7 +272,7 @@ def main():
         dest='kafkahost',
         type = str,
         help='Kafka Host - monitoring server; must be specified for delete operation',
-        default='18.223.195.153:9092',
+        default='dynamo-broker1.exogeni.net:9093',
         required=False
     )
 
@@ -746,17 +746,85 @@ def cleanup_monitoring(mb, mobiushost, workflowId, kafkahost, response):
                         hostname=n["name"]
                     if n["name"] == "cmnw" :
                         continue
-                    topics.append(workflowId + hostname)
+                    topics.append("mobius-" + workflowId + "-" + hostname)
     delete_kafka_topic(topics, kafkahost)
 
 def delete_kafka_topic(topics, kafkahost):
     try:
         print ("List of topics to be deleted " + str(topics))
-        a = KafkaAdminClient(bootstrap_servers=[kafkahost])
+        context = _create_ssl_context(cafile="certs/DigiCertCA.crt", certfile="certs/inno-hn_exogeni_net.pem", keyfile="certs/inno-hn_exogeni_net.key")
+        context.check_hostname = False
+        context.verify_mode = False
+        a = KafkaAdminClient(bootstrap_servers=[kafkahost],
+                             security_protocol='SSL',
+                             ssl_context=context)
         a.delete_topics(topics, timeout_ms=30)
     except Exception as e:
-        #print("Exception occured while deleting topics e=" + str(e))
+        print("Exception occured while deleting topics e=" + str(e))
         print(" ")
+
+
+def _create_ssl_context(cafile=None, capath=None, cadata=None,
+                       certfile=None, keyfile=None, password=None,
+                       crlfile=None):
+    """
+    Simple helper, that creates an SSLContext based on params similar to
+    those in ``kafka-python``, but with some restrictions like:
+
+            * ``check_hostname`` is not optional, and will be set to True
+            * ``crlfile`` option is missing. It is fairly hard to test it.
+
+    .. _load_verify_locations: https://docs.python.org/3/library/ssl.html\
+        #ssl.SSLContext.load_verify_locations
+    .. _load_cert_chain: https://docs.python.org/3/library/ssl.html\
+        #ssl.SSLContext.load_cert_chain
+
+    Arguments:
+        cafile (str): Certificate Authority file path containing certificates
+            used to sign broker certificates. If CA not specified (by either
+            cafile, capath, cadata) default system CA will be used if found by
+            OpenSSL. For more information see `load_verify_locations`_.
+            Default: None
+        capath (str): Same as `cafile`, but points to a directory containing
+            several CA certificates. For more information see
+            `load_verify_locations`_. Default: None
+        cadata (str/bytes): Same as `cafile`, but instead contains already
+            read data in either ASCII or bytes format. Can be used to specify
+            DER-encoded certificates, rather than PEM ones. For more
+            information see `load_verify_locations`_. Default: None
+        certfile (str): optional filename of file in PEM format containing
+            the client certificate, as well as any CA certificates needed to
+            establish the certificate's authenticity. For more information see
+            `load_cert_chain`_. Default: None.
+        keyfile (str): optional filename containing the client private key.
+            For more information see `load_cert_chain`_. Default: None.
+        password (str): optional password to be used when loading the
+            certificate chain. For more information see `load_cert_chain`_.
+            Default: None.
+
+    """
+    if cafile or capath:
+        print('Loading SSL CA from %s', cafile or capath)
+    elif cadata is not None:
+        print('Loading SSL CA from data provided in `cadata`')
+        print('`cadata`: %r', cadata)
+    # Creating context with default params for client sockets.
+    context = create_default_context(
+        Purpose.SERVER_AUTH, cafile=cafile, capath=capath, cadata=cadata)
+    # Load certificate if one is specified.
+    if certfile is not None:
+        print('Loading SSL Cert from %s', certfile)
+        if keyfile:
+            if password is not None:
+                print('Loading SSL Key from %s with password', keyfile)
+            else:  # pragma: no cover
+                print('Loading SSL Key from %s without password', keyfile)
+        # NOTE: From docs:
+        # If the password argument is not specified and a password is required,
+        # OpenSSLs built-in password prompting mechanism will be used to
+        # interactively prompt the user for a password.
+        context.load_cert_chain(certfile, keyfile, password)
+    return context
 
 if __name__ == '__main__':
     main()
